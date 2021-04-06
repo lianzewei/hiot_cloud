99c56b69e615c4a036e28b451f21b82fefd20e6d
yudengyu <1574407378@qq.com>
2021-04-06 16:43:50 +0800

    添加MVP测试

--- app/src/main/java/com/example/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/example/hiot_cloud/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("tag", "test");
+       EditText etUserName = findViewById(R.id.et_user_name);
+       EditText etPassword = findViewById(R.id.et_password);
+        Button btnlogin = findViewById(R.id.btn_login);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
