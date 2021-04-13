07a3afc5a8548be34134463ca5352da933aaf030
yudengyu <1574407378@qq.com>
2021-04-13 10:59:20 +0800

    更新BaseActivity,并测试

--- app/src/main/java/com/example/hiot_cloud/test/mvptest/TestMVPActivity.java
+++ app/src/main/java/com/example/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -24,7 +24,7 @@ public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> imple
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        getComponent().inject(this);
+        getActivityComponent().inject(this);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_test_mvp);
@@ -52,14 +52,6 @@ public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> imple
     @Override
     public void showMessage(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * 创建注入器
-     * @return
-     */
-    public PresenterComponent getComponent(){
-        return DaggerPresenterComponent.builder().build();
     }
 
 }
